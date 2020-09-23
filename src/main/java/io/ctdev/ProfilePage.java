7cb8491004c6bc79951793ec20c41d8dec1a61e0
bohdan.bulazhenko <bohdanbulazhenko@gmail.com>
2020-09-23 16:05:13 +0300

    Initial commit

---
+++ src/main/java/io/ctdev/ProfilePage.java
@@ -0,0 +1,28 @@
+package io.ctdev;
+
+import java.lang.String;
+
+public class ProfilePage {
+
+    //CSS Locators
+    private final String CROSS_BUTTON = "button.react-modal-close-btn";
+    private final String UPLOAD_IMAGE = "p.editUserProfile_dropZoneHover__3MCjI";
+    private final String NAME = "input[name='name']";
+    private final String NICKNAME = "input[name='login']";
+    private final String PHONE_NUMBER = "input[name='phoneNumber']";
+    private final String POSITION = "input[name='position']";
+    private final String DEPARTMENT = "input[name='department']";
+    private final String CANCEL_BUTTON = "button.button_btn__2GBfK.editUserProfile_cancelBtn__fnKLZ";
+    private final String SAVE_BUTTON = "button.button_btn__2GBfK[type='submit']";
+
+    //Xpath Locators
+    private final String CROSS_BUTTON = "//button[@class='react-modal-close-btn']";
+    private final String UPLOAD_IMAGE = "//p[@class='editUserProfile_dropZoneHover__3MCjI']";
+    private final String NAME = "//input[@name='name']";
+    private final String NICKNAME = "//input[@name='login']";
+    private final String PHONE_NUMBER = "//input[@name='phoneNumber']";
+    private final String POSITION = "//input[@name='position']";
+    private final String DEPARTMENT = "//input[@name='department']";
+    private final String CANCEL_BUTTON = "//button[@class='button_btn__2GBfK editUserProfile_cancelBtn__fnKLZ']";
+    private final String SAVE_BUTTON = "//button[@class='button_btn__2GBfK'][@type='submit'];
+}
